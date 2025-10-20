4283ff3933b6aee2656ed5a334511241039e2ec9
Usmaan <usmaanahmed2004@gmail.com>
2025-10-20 19:19:19 +0100

    Initial Commit

---
+++ Mod Garage/Mod_GarageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod_GarageApp.swift
+//  Mod Garage
+//
+//  Created by Usmaan Ahmed on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mod_GarageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
